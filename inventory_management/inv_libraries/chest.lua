8bd45027831b040376defde5ba8141448f17208e
Sean Fitzgerald <sfitzgerald004@gmail.com>
2026-04-03 11:41:52 -0400

    Fixed function override

--- inventory_management/inv_libraries/chest.lua
+++ inventory_management/inv_libraries/chest.lua
@@ -1,27 +1,25 @@
 -- chest.lua
 
 -- iterate through and count every slot
-function read(chest)
+local function read_chest(chest)
     if chest == nil or type(chest.size) ~= "function" or type(chest.list) ~= "function" then
         return nil
     end
 
     local current = 0
-    local percentage = 0
     local max = chest.size() * 64
 
-    for slot, item in pairs(chest.list()) do
+    for _, item in pairs(chest.list()) do
         current = current + item.count
     end
 
-    percentage = (current / max) * 100
-    local fraction = (current.. "/".. max)
+    local percentage = (current / max) * 100
+    local fraction = (current .. "/" .. max)
     local percentage_used = string.format("%.1f %%", percentage)
 
     return {
         name = peripheral.getName(chest),
-        -- location not done yet
-        capacity_used = percentage_used.. " (".. fraction.. ")",
+        capacity_used = percentage_used .. " (" .. fraction .. ")",
         percentage = percentage,
         fields = {
             { key = "name", label = "Name" },
