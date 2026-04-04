b2d00bc8378980d26b12183722183588f765b546
Sean Fitzgerald <scfitzgerald004@gmail.com>
2026-04-04 19:53:09 -0400

    Removed commented section from chest.lua

--- inventory_management/inv_libraries/chest.lua
+++ inventory_management/inv_libraries/chest.lua
@@ -35,34 +35,3 @@ local function read_chest(inventory_name)
 end
 
 return read_chest
-
---[[
-local function read_chest(chest)
-    if chest == nil or type(chest.size) ~= "function" or type(chest.list) ~= "function" then
-        return nil
-    end
-
-    local current = 0
-    local max = chest.size() * 64
-
-    for _, item in pairs(chest.list()) do
-        current = current + item.count
-    end
-
-    local percentage = (current / max) * 100
-    local fraction = (current .. "/" .. max)
-    local percentage_used = string.format("%.1f %%", percentage)
-
-    return {
-        name = peripheral.getName(chest),
-        capacity_used = percentage_used .. " (" .. fraction .. ")",
-        percentage = percentage,
-        fields = {
-            { key = "name", label = "Name" },
-            { key = "capacity_used", label = "Capacity Used" }
-        }
-    }
-end
-
-return read_chest
-]]
