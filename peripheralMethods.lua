bf65551b1204cad0f1cbe477f923b7a4ca2ce5b7
Sean Fitzgerald <sfitzgerald004@gmail.com>
2026-04-04 14:00:13 -0400

    Adding extra stuff

--- peripheralMethods.lua
+++ peripheralMethods.lua
@@ -3,14 +3,16 @@
 
 local sides = {"front", "back", "bottom", "top", "left", "right"} -- six sides in total
 local peripherals = {}
+local currentPeriph
 
 for index, side in ipairs(sides) do
     print(index, side)
     -- checks if anything is attached to the given side
     if peripheral.isPresent(side) then
         print(" !- Peripheral found on side", side)
-        print(" !-", peripheral.getName())
-        table.insert(peripherals, peripheral.getName(side))
+        currentPeriph = peripheral.wrap(side)
+        print(" !-", peripheral.getName(currentPeriph))
+        table.insert(peripherals, currentPeriph)
     else
         print(" - No peripheral found on side", side)
     end
