21a185c272a31c88b05484356db95eccfc4eed01
Sean Fitzgerald <scfitzgerald004@gmail.com>
2026-04-04 19:53:16 -0400

    added tank.lua

---
+++ inventory_management/inv_libraries/tank.lua
@@ -0,0 +1,11 @@
+-- tank.lua
+
+-- gather tank information
+local function read_tank(inventory_name)
+    if type(inventory_name) ~= "string" then
+        return nil
+    end
+
+end
+
+return read_tank
