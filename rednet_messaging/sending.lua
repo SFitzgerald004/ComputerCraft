fd4151cc87e0fa4be7310bbe2c875653272019c1
Sean Fitzgerald <sfitzgerald004@gmail.com>
2026-06-16 18:09:22 -0400

    Added simple sending program

---
+++ rednet_messaging/sending.lua
@@ -0,0 +1,21 @@
+-- sending.lua
+local modem = peripheral.find("modem")
+
+if not modem then
+    error("No modem attached.", 0)
+end
+
+-- opens rednet on the modem; checks if wireless first
+if not modem.isWireless() then
+    if rednet.isOpen() then
+        print("Rednet already opened")
+    else
+        print("Opening rednet")
+        rednet.open("top") -- modem required to be on top
+    end
+else
+    peripheral.find("modem", rednet.open)
+end
+
+local message = io.read()
+rednet.broadcast(message)
